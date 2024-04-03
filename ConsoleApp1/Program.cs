2235889e7caf2af430456f5569f562e60d1579e9
217546 <217546@KB37-1-120-C02>
2024-04-03 14:14:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static int ResultCalc(int score_1, int score_2, int score_3)
+        {
+            int result = score_1 + score_2 + score_3;
+            return result;
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            int score_1;
+            int score_2;
+            int score_3;
+
+            Console.WriteLine("1 Score:");
+            score_1 = Int32.Parse(Console.ReadLine());
+            if (score_1 > 22)
+            {
+                Console.WriteLine("Больше диапазона. 22 max");
+                return;
+            }
+
+            Console.WriteLine("2 Score:");
+            score_2 = Int32.Parse(Console.ReadLine());
+            if (score_2 > 38)
+            {
+                Console.WriteLine("Больше диапазона. 38 max");
+                return;
+            }
+
+            Console.WriteLine("3 Score:");
+            score_3 = Int32.Parse(Console.ReadLine());
+            if (score_3 > 20)
+            {
+                Console.WriteLine("Больше диапазона. 20 max");
+                return;
+            }
+
+            int result = ResultCalc(score_1, score_2, score_3);
+
+            Console.WriteLine("Result Score:");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace ConsoleApp1UnitTest
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestResultCalc()
+        {
+            int score_1 = 5;
+            int score_2 = 10;
+            int score_3 = 15;
+
+            int expectedResult = score_1 + score_2 + score_3;
+
+            int actualResult = Program.ResultCalc(score_1, score_2, score_3);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestScore1InRange()
+        {
+            int score_1 = 20;
+
+            bool result = score_1 >= 0 && score_1 <= 22;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestScore2InRange()
+        {
+            int score_2 = 30; 
+
+            bool result = score_2 >= 0 && score_2 <= 38;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestScore3InRange()
+        {
+            int score_3 = 18; 
+
+            bool result = score_3 >= 0 && score_3 <= 20;
+
+            Assert.IsTrue(result);
+        }
+    }
+}
